ce8b45b75ed42b9c901d91ab5c14fa47d028244f
3dcantaloupe <66147967+3dcantaloupe@users.noreply.github.com>
2021-05-12 17:53:24 -0700

    setup

---
+++ naturalDisaster/src/main/java/zen/cantaloupe/disaster/naturalDisaster.java
@@ -0,0 +1,17 @@
+package zen.cantaloupe.disaster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class naturalDisaster extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
