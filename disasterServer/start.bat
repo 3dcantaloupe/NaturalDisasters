8ecd2a0973b2bcce2a8ba642049980be098e56d0
3dcantaloupe <66147967+3dcantaloupe@users.noreply.github.com>
2021-05-13 08:58:04 -0700

    Added the test server and helloworld plugin

---
+++ disasterServer/start.bat
@@ -0,0 +1,2 @@
+java -Xmx4096M -Xms4096M -jar spigot-1.16.5.jar
+pause
--- naturalDisaster/src/main/java/zen/cantaloupe/disaster/naturalDisaster.java
+++ naturalDisaster/src/main/java/zen/cantaloupe/disaster/naturalDisaster.java
@@ -6,7 +6,7 @@ public final class naturalDisaster extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("Hello World!");
 
     }
 
